334d4f029a084133c1a2cb9ac055381ce8de0e5b
SOUMITRA-SAHA <soumitrosahaofficial@gmail.com>
2025-04-25 09:45:34 +0530

    Re-factoring

--- init.lua
+++ init.lua
@@ -1,12 +1,2 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-
--- show hidden files
-vim.cmd('let g:neo_tree_show_hidden = 1')
-
--- NOTE: 
--- TODO: 
--- BUG: 
--- FIX:
--- WI
--- XXX:
